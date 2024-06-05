ad2c1a0ff823c6654b588cf5a74182da1ba9e728
Hideki Maesaka <lucasmsk11@gmail.com>
2024-06-05 17:42:49 -0300

    add new func

--- MusicPriceTracker/src/test/java/com/expeditors/musicpricetracker/service/MusicPriceServiceTest.java
+++ MusicPriceTracker/src/test/java/com/expeditors/musicpricetracker/service/MusicPriceServiceTest.java
@@ -3,13 +3,18 @@ package com.expeditors.musicpricetracker.service;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.expeditors.musicpricetracker.service.MusicPriceService;
 
 @SpringBootTest
 public class MusicPriceServiceTest {
 
+    public MusicPriceService musicPriceService;
+
     @Test
     public void testGetTrackPriceWithoutIdException(){
+        double price = musicPriceService.generatePrice();
 
+        System.out.println(price);
     }
 
     @Test
